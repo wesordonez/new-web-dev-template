6730e47bca9137e3378d3a43fafe1a5b2930d333
Wesley Ordoñez <wesordonez1@gmail.com>
2025-04-24 18:38:47 -0500

    installed tailwind 4.1

--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Third-party apps
+    'tailwind',
+    'styling',
+    'django_browser_reload',
 ]
+
+TAILWIND_APP_NAME = 'styling'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -47,6 +54,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
 ]
 
 ROOT_URLCONF = 'project.urls'
